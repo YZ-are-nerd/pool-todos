14587a0098d5aff338e3857d42a1f5964a9b30e4
One Above All <101273441+YZ-are-nerd@users.noreply.github.com>
2022-12-26 15:27:15 +0500

    Скрывает некоторые элементы на PC-версии

--- src/components/global/mobilebar/molecules/UserBar.tsx
+++ src/components/global/mobilebar/molecules/UserBar.tsx
@@ -14,7 +14,7 @@ const UserBar = () => {
         navigate('/auth')
     }
     return (
-        <div className="w-full h-10 py-1 px-2 mt-auto rounded-xl flex items-center justify-between bg-neutral-800">
+        <div className="w-full h-10 py-1 px-2 mt-auto rounded-xl lg:hidden md:hidden flex items-center justify-between bg-neutral-800">
             <div className="w-fit h-full flex items-center gap-2">
                 <UserAvatar />
                 <p className="font-bold text-neutral-300">{user?.username}</p>
